472b775300ed72b78d3782f70131ba589315c46b
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2022-08-30 12:23:28 +0200

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DashboardComponent} from "./admin/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'admin/dashboard', component:DashboardComponent},
+  {path: '', component:HomeComponent},
+  {path:'**', component: HomeComponent, pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
